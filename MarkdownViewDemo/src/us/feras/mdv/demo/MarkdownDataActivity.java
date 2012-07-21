404a305552ec0a5975a17e94ea0ea72b902b18fc
Feras Alnatsheh <contact@feras.us>
2012-07-21 10:43:31 -0400

    Change the background color for an example.

--- MarkdownViewDemo/src/us/feras/mdv/demo/MarkdownDataActivity.java
+++ MarkdownViewDemo/src/us/feras/mdv/demo/MarkdownDataActivity.java
@@ -2,6 +2,7 @@ package us.feras.mdv.demo;
 
 import us.feras.mdv.MarkdownView;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,6 +22,7 @@ public class MarkdownDataActivity extends Activity {
 		markdownView = (MarkdownView) findViewById(R.id.markdownView);
 		String text = "## This is a demo of MarkdownView" + "\n" + "* * *" + "\n" + "### Edit the text and hit update to see the changes on the view.";
 		markdownEditText.setText(text);
+		markdownView.setBackgroundColor(Color.parseColor("#EED8AE"));
 		updateMarkdownView();
 
 		Button updateView = (Button) findViewById(R.id.updateButton);
