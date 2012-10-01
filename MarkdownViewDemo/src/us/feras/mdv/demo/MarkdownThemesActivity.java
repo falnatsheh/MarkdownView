4900c713f93456063824819085acc574dbc48f69
Feras Alnatsheh <me@feras.us>
2012-09-30 20:44:54 -0400

    Added themes activity

---
+++ MarkdownViewDemo/src/us/feras/mdv/demo/MarkdownThemesActivity.java
@@ -0,0 +1,11 @@
+package us.feras.mdv.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MarkdownThemesActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+	}
+}
